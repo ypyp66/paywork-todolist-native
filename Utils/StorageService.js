02c07c33aa2a4e06dc791576c7ffcaf67620d71a
Yeonbeen <ypyp66@naver.com>
2021-09-01 15:06:12 +0900

    Storage함수 수정중

--- Utils/StorageService.js
+++ Utils/StorageService.js
@@ -1,13 +1,7 @@
 import { STORAGE_NAME } from "../Constants/StorageName";
-
 const Storage = {
-  set: (todos) => {
-    localStorage.setItem(STORAGE_NAME, JSON.stringify(todos));
-  },
-  get: () => {
-    const data = localStorage.getItem(STORAGE_NAME);
-    console.log(data);
-  },
+  set: (todos) => {},
+  get: () => {},
 };
 
 export default Storage;
